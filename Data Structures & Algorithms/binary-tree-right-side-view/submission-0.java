94172c10a3dd4fcb1a40fbdea57248a9376acf9b
Rohti negi <140068684+DarkTHor55@users.noreply.github.com>
2026-07-30 22:27:45 +0530

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null)return new ArrayList<>();
+        List<List<Integer>>ls=new ArrayList<>();
+        Queue<TreeNode>queue=new LinkedList<>();
+        queue.offer(root);
+        queue.offer(null);
+        List<Integer>l=new ArrayList<>();
+        l.add(root.val);
+        ls.add(l);
+        while (!queue.isEmpty()) {
+            List<Integer>tl=new ArrayList<>();
+            while (queue.peek()!=null) {
+                TreeNode node=queue.remove();
+                if(node.left!=null) {
+                    tl.add(node.left.val);
+                    queue.add(node.left);
+                }
+                if(node.right!=null) {
+                    tl.add(node.right.val);
+                    queue.add(node.right);
+                }
+            }
+
+            if(queue.peek()==null) {
+                queue.remove();
+                if (queue.isEmpty()) break;
+                queue.offer(null);
+            }
+            ls.add(tl);
+        }
+        for(List<Integer> list:ls) {
+            if(!list.isEmpty()) {
+                l.add(list.get(list.size() - 1));
+            }
+        }
+        l.remove(0);
+        return l;
+    }
+}
